bc1d0aa2931d8a314289afee79b5534254700f88
Roman Gromov <GromRom1289@yandex.ru>
2022-01-17 20:49:03 +0300

    React1

---
+++ React1/homeworh-1/src/homework-1/index.js
@@ -0,0 +1,13 @@
+import { Message } from "./components";
+
+const text = 'completed'
+
+export const Homework = () => {
+    return (
+        <div>
+            <h1>Homework 1</h1>
+            <Message text={text} />
+        </div>
+    );
+    ;
+}
